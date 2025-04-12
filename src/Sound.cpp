84239a8c21dffe511b0f8a77594e79055cee7410
Scott Ferguson <scottferg@outlook.com>
2025-04-11 22:11:40 -0700

    sound list and volume properties

--- src/Sound.cpp
+++ src/Sound.cpp
@@ -28,6 +28,18 @@ void SoundConnector::Push(OMObject* obj, OMProperty* prop)
             sound->Play("Cheerful R2D2.mp3");
         }
         break;
+    case 'l':   // sound file list
+        {
+            floge("sound list should not be pushed to device");
+        }
+        break;
+    case 'v':   // volume
+        {
+
+            int volume = ((OMPropertyLong*)prop)->Value;
+            audio.setVolume(volume);
+        }
+        break;
     }
 }
 
@@ -40,6 +52,46 @@ void SoundConnector::Pull(OMObject *obj, OMProperty *prop)
     case 'p':   // play
         // UNDONE:
         ((OMPropertyLong*)prop)->Value = 0;
+        break;
+    case 'l':   // sound file list
+        {
+            String s;
+            File root = SPIFFS.open("/", 0);
+            if (!root)
+            {
+                floge("Failed to open root directory");
+                return;
+            }
+            File file = root.openNextFile();
+            while (file)
+            {
+                if (!file.isDirectory())
+                {
+                    String name(file.name());
+                    name.toLowerCase();
+                    if (name.endsWith(".mp3") || name.endsWith(".MP3"))
+                    {
+                        name.remove(name.length() - 4);
+                        // good enough for about 20 sound files in an esp_now packet
+                        if (name.length() + s.length() > 240)
+                        {
+                            floge("sound list too long");
+                            break;
+                        }
+                        if (s.length() > 0)
+                            s.concat(',');
+                        s.concat(name);
+                    }
+                }
+                file = root.openNextFile();
+            }
+            ((OMPropertyString*)prop)->Value = s;
+        }
+        break;
+    case 'v':   // volume
+        {
+            ((OMPropertyLong*)prop)->Value = audio.getVolume();
+        }
         break;
     }
 }
