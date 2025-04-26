939b073b6e98289d80d9dacff1a6687bc6af3bc3
Scott Ferguson <scottferg@outlook.com>
2025-04-25 22:56:58 -0700

    remove setup DIR listing

--- src/main.cpp
+++ src/main.cpp
@@ -143,37 +143,6 @@ int flog_printer(const char* s)
     return len;
 }
 
-void listDir(fs::FS &fs, const char * dirname, uint8_t levels){
-    Serial.printf("Listing directory: %s\n", dirname);
-  
-    File root = fs.open(dirname);
-    if(!root){
-      Serial.println("Failed to open directory");
-      return;
-    }
-    if(!root.isDirectory()){
-      Serial.println("Not a directory");
-      return;
-    }
-  
-    File file = root.openNextFile();
-    while(file){
-      if(file.isDirectory()){
-        Serial.print("  DIR : ");
-        Serial.println(file.name());
-        if(levels){
-          listDir(fs, file.name(), levels -1);
-        }
-      } else {
-        Serial.print("  FILE: ");
-        Serial.print(file.name());
-        Serial.print("  SIZE: ");
-        Serial.println(file.size());
-      }
-      file = root.openNextFile();
-    }
-  }
-
 void setup()
 {
     // nvs_flash_erase();      // erase the NVS partition and...
@@ -188,12 +157,6 @@ void setup()
     if (!SPIFFS.begin())
     {
         floge("SPIFFS init error");
-    }
-    else
-    {
-        listDir(SPIFFS, "/", 0);
-        Serial.printf("Total space: %lu\n", SPIFFS.totalBytes());
-        Serial.printf("Used space: %lu\n", SPIFFS.usedBytes());
     }
 
     agent.Setup(peerMacAddress);
